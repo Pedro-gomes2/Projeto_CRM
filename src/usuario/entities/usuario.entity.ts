bfb0cea67e2d868292f2877437f80cbf5a9c1c84
Lary <larissa.mendy@gmail.com>
2026-03-17 09:10:25 -0300

    ✨ New Feature: Criação de Usuario

---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,55 @@
+import { Transform, TransformFnParams } from "class-transformer"
+import { IsDateString, IsEmail, IsNotEmpty, MinLength } from "class-validator"
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+
+import { Oportunidade } from "../../oportunidade/entities/oportunidade.entity"
+
+
+@Entity({name: "tb_usuarios"})
+export class Usuario {
+
+    @PrimaryGeneratedColumn() 
+    id: number
+    
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false}) 
+    nome: string
+
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsEmail()
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false })
+    usuario: string
+
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @MinLength(8)
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false }) 
+    senha: string
+
+    @Column() 
+    foto: string
+
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsDateString()
+    @Column({type: "date"})
+    dataNascimento: Date
+
+    @CreateDateColumn({ type: "timestamp" })
+    dataCriacao: Date
+
+    
+    //Relacionamento com Oportunidade
+     @OneToMany(() => Oportunidade,(oportunidade) => oportunidade.usuario)
+    oportunidade: Oportunidade[];
+
+
+
+   
+
+} 

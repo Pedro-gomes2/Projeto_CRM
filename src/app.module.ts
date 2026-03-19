e13afa808e4c7685a9a58f055789c56930eefcc5
Joao <oliveira110965@gmail.com>
2026-03-18 21:16:56 -0300

    Deploy do Projeto CRM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OportunidadeModule } from './oportunidade/oportunidade.module';
 import { Oportunidade } from './oportunidade/entities/oportunidade.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { CandidataModule } from './candidata/candidata.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/prod.service';
 
 @Module({
-  imports: [ TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'db_crmproject',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //Synchronize é para sincronizar as funções, em produção deixamos a opção em false
-    synchronize: true,
-  }), OportunidadeModule, UsuarioModule, CandidataModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
+
+    }),
+    OportunidadeModule,
+    UsuarioModule,
+    CandidataModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
